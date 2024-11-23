012183e9a0138e31108f4b6339c0abbc79872ccb
Rahul <rahulpatil989018@gmail.com>
2024-11-23 08:18:01 +0530

    align code

--- src/util/generateUniqueName.js
+++ src/util/generateUniqueName.js
@@ -1,8 +1,10 @@
 const { v4: uuidv4 } = require('uuid');
 
 // Function to generate a unique name for the image
-export const  generateUniqueImageName =(originalFileName)=> {
+const generateUniqueName =(originalFileName)=> {
   const fileExtension = originalFileName.split('.').pop(); // Extract the file extension
   const uniqueName = `${uuidv4()}.${fileExtension}`;       // Combine UUID with file extension
   return uniqueName;
 }
+
+module.exports = generateUniqueName;
